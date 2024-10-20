5e80efeef76d2a619032b4f4da7996fefda2df31
pavan kumar k a <pshimoga@gmail.com>
2024-10-20 17:42:24 +0530

    complexCompareBitwiseShiftOptrAdded

---
+++ 1_Python_for_DS/3_complex_dataTypes.py
@@ -0,0 +1,119 @@
+#addition
+a = 4
+b = 5
+c = a + b
+print(a + b)
+print(c)
+print(a+b, c)
+print(4 + 5, c, c)
+A = "Pavan"
+B = "Kumar"
+print(A,B)
+print(A + B)
+print(True + False)
+print(True + True)
+#print("Pavan" + 100) #illegal
+print("Pavan" + "100")
+print( c + 10.5)
+
+#subtraction
+c = a - b
+print(a - b)
+print(c)
+print(a-b, c)
+print(4 - 5, c, c)
+print( c - 10.5)
+A = "Pavan"
+B = "Kumar"
+print(A,B)
+#print(A - B)
+print(True - False)
+print(True - True)
+#print("Pavan" - 100) #illegal
+#print("Pavan" - "100")
+
+#Multiplication
+c = a * b
+print(a * b)
+print(c)
+print(a * b, c)
+print(4 * 5, c, c)
+print( c * 10.5)
+A = "Pavan"
+B = "Kumar"
+print(A,B)
+#print(A * B) #not allowed
+print(True * False)
+print(True * True)
+#print("Pavan" * 100) #illegal
+#print("Pavan" * "100")
+
+#Division
+c = a / b
+print(a / b)
+print(c)
+print(a / b, c)
+print(4 / 5, c, c)
+print( c / 10.5)
+A = "Pavan"
+B = "Kumar"
+print(A,B)
+#print(A / B) #not allowed
+#print(True / False) #division by zero not allowed
+print(True / True)
+#print("Pavan" / 100) #illegal
+#print("Pavan" / "100") #illegal
+
+#Floor division
+a = 3
+b = 2
+c = a // b
+print(a,b,c)
+print( 3 // 2)
+print( a // b, a, b, c)
+print( 3.5 // 2.5)
+print( 2.5 // 3.5)
+
+#Modulus division
+a = 5
+b = 4
+c = a % b
+print(a,b,c)
+print( 3 % 2)
+print( a % b, a, b, c)
+print( 3.5 % 2.5)
+print( 2.5 % 3.5)
+print("--------------------")
+#Exponential
+a = 5
+b = 4
+print( 5 * 5 * 5 * 5)
+c = a ** b
+print(a,b,c)
+print( 3 ** 2)
+print( a ** b, a, b, c)
+print( 3.5 ** 2.5)
+print( 2.5 ** 3.5)
+#print( "2.5 ** 3.5 = ", 2.5 * 2.5 * 2.5 * (2.5/2) ) #check how to solve
+print("--------------------")
+
+#absolute
+print(abs(-3.5))
+print(abs(-45))
+print(abs(-3.5/2.5))
+
+#round
+print(round(-3.5))
+print(round(-45))
+print(round(3.5/2.5))
+print(round(3.52345677777, 2))
+print(round(3.52345677777, 3))
+print(round(3.52345677777, 5))
+print(round(-3.000000007, 4))
+
+#pow
+print(pow(2,3))
+print(pow(2.0, 3.0))
+print(pow(2.0, 3))
+print(pow(-2.0, 3.0))
+print(pow(2.0, -3.0))
---
+++ 1_Python_for_DS/5_bitwise_operator.py
@@ -0,0 +1,27 @@
+# AND
+print( True and True)
+print( True and False)
+print( False & True)
+print( False & False)
+print("-----------------")
+
+# OR
+print( True or True)
+print( True or False)
+print( False | True)
+print( False | False)
+print("-----------------")
+
+# NOT
+print( not True)
+print( not False)
+print( False & (not True))
+print( False | (not False))
+print("-----------------")
+
+# XOR
+print( True ^ True)
+print( True ^ False)
+print( False ^ True)
+print( False ^ False)
+print("-----------------")
